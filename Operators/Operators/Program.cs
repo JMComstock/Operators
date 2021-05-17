65b1bd84d9e7d080ad5d6e7f018cf82c28fc77af
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-05-17 09:15:44 -0400

    Add files via upload

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            Console.WriteLine(a + b);
+
+            var c = 10;
+            var d = 3;
+            Console.WriteLine((float)c / (float)d);
+
+            var e = 1;
+            var f = 2;
+            var g = 3;
+            Console.WriteLine(e + f * g);
+            Console.WriteLine((e + f) * g);
+
+
+        }
+    }
+}
